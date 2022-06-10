0e4122b74f6b6f5b3c448af8088e211bb193675e
Jason <651705864@qq.com>
2022-06-10 19:38:01 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GiTest.java
+++ src/main/java/com/atguigu/git/GiTest.java
@@ -11,5 +11,6 @@ public class GiTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hot-fix test!");
     }
 }
