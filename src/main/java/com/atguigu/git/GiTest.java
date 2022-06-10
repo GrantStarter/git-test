a6710d1d701e3907795c367f72fea2bc662f44b2
Jason <651705864@qq.com>
2022-06-10 19:25:12 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GiTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author NiceChen
+ * @create 2022-06-10 7:18 PM
+ */
+public class GiTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
