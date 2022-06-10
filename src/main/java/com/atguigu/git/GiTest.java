f59d08fa4f199ea7fa89a69bb196e2c3fde35800
GrantStarter <85844878+GrantStarter@users.noreply.github.com>
2022-06-10 19:57:58 +0800

    pull commit

--- src/main/java/com/atguigu/git/GiTest.java
+++ src/main/java/com/atguigu/git/GiTest.java
@@ -14,6 +14,7 @@ public class GiTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
 
     }
 }
